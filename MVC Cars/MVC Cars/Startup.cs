8d1ce429b7f6df6b13025176bdeca1d8e612e4cc
ali sina <ali@mra.li>
2016-04-05 09:43:23 +0200

    First

---
+++ MVC Cars/MVC Cars/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Cars.Startup))]
+namespace MVC_Cars
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
